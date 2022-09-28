8b77a3df2c34e9d25458953ae3159965573cff4a
Salman S <salkuadrat@gmail.com>
2022-09-28 15:30:21 +0700

    move all command handlers to module

--- command.go
+++ command.go
@@ -1,3 +1,67 @@
 package git4humans
 
+import (
+	"fmt"
+	"os"
+)
+
 var Command = "gh"
+
+func Handle() {
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		Execute()
+	} else {
+		Help()
+	}
+}
+
+func Execute() {
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	switch command {
+	case "new":
+		New()
+	case "+":
+		Add()
+	case "-":
+		Delete()
+	case "del":
+		Delete()
+	case "delete":
+		Delete()
+	case "rename":
+		Move()
+	case "move":
+		Move()
+	case "copy":
+		Copy()
+	case "save":
+		Save()
+	case "push":
+		Push()
+	case "sync":
+		Sync()
+	case "branch":
+		Branch()
+	case "switch":
+		SwitchBranch()
+	case "repo":
+		Repo()
+	case "pr":
+		Pr()
+	case "user":
+		User()
+	case "help":
+		Help()
+	default:
+		if Contains(GitCommands, command) {
+			Git(command, args...)
+		} else {
+			fmt.Printf(`Error: '%[1]s' is not a valid command. See %[2]s help.
+            `, command, Command)
+		}
+	}
+}
