563680d7f61ba54266c2282773004507bbbc7428
Salman S <salkuadrat@gmail.com>
2022-09-30 01:21:22 +0700

    update add functionality

--- add.go
+++ add.go
@@ -1,8 +1,10 @@
 package git4humans
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Add untracked files into staging.
@@ -21,11 +23,37 @@ func Add() {
 
 	if len(args) > 0 {
 		Git("add", args...)
+		Git("status")
 	} else {
-		Git("add", ".")
-	}
+		files := ""
+		reader := bufio.NewReader(os.Stdin)
 
-	Git("status")
+		fmt.Print("\nFile(s) to add: ")
+
+		input, _, _ := reader.ReadLine()
+
+		if len(input) > 0 {
+			files = strings.Replace(string(input), "\n", "", -1)
+		}
+
+		if len(files) > 0 {
+			Git("add", strings.Fields(files)...)
+			Git("status")
+		} else {
+			fmt.Printf(`
+You should specify file(s) to add.
+
+Examples:
+    
+    %[1]s + file.txt
+    %[1]s + file1.txt file2.txt file3.txt
+    %[1]s + file1.txt dir2/file2.txt
+    %[1]s + file.txt dir/
+	%[1]s + --all 
+	%[1]s + .
+            `, Command)
+		}
+	}
 }
 
 func AddUsage() {
@@ -36,8 +64,10 @@ usage: %[1]s + [<file>]
 
 examples:
     %[1]s + file.txt
-    %[1]s + file1.txt file2.txt
+    %[1]s + file1.txt file2.txt file3.txt
     %[1]s + file1.txt dir2/file2.txt
     %[1]s + file.txt dir/
+	%[1]s + --all
+	%[1]s + .
 	`, Command)
 }
