bc87e04e9433a5e4cd1ca070b55b1d75e9932ec7
Salman S <salkuadrat@gmail.com>
2022-09-29 14:47:30 +0700

    update help command

--- help.go
+++ help.go
@@ -3,5 +3,49 @@ package git4humans
 import "fmt"
 
 func Help() {
-	fmt.Println("Git for Humans " + Version)
+	fmt.Printf(`
+Git for Humans version %[1]s
+
+usage: gh [<options>] <command> [<args]
+
+These are common commands you will most likely use in everyday situation.
+
+Start a working area
+    copy  	Copy an existing repository to a new directory
+    new		Create an Git repository in a new directory 
+    start	Initialize an fresh Git repository in the current working directory
+
+Work on the current change 
+    + 		Add file contents to the index 
+    - 		Delete file(s) from the directory and from the index 
+    move 	Move a file, a directory, or a symlink 
+    rename 	Rename a file, a directory, or a symlink 
+    restore	Restore deleted file(s)
+    save 	Record changes to the repository
+
+Collaborate with others
+    repo 	List, add, or delete remote repositories
+    sync 	Fetch from and integrate with another repository or a local branch 
+    submit 	Update changes into remote repository 
+    fetch 	Download from another repository
+
+Grow, mark, and tweak your common history 
+    branch 	List, create, or delete branches 
+    switch 	Switch to another branch 
+    merge 	Join two or more branches 
+    reset 	Reset current HEAD to a specified state
+    tag 	Create, list, delete, or verify a tag object
+
+Examine history and state 
+    status 	Show the working tree status 
+    log 	Show commit logs 
+    diff 	Show changes between commits, commit and working tree, etc.
+
+Configurations 
+    config	Show or update Git configuration
+    user	Show or update Git user configuration (local or global)
+
+Also, you can use other standard commands that comes with Git. 
+As an example, 'gh push origin main' will work exactly as 'git push origin main' 
+	`, Version)
 }
