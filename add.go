8ab34d93ea94b366ea083e0cf341db6dd9fb677a
Salman S <salkuadrat@gmail.com>
2022-09-29 17:14:27 +0700

    add usage for command +

--- add.go
+++ add.go
@@ -1,10 +1,18 @@
 package git4humans
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 // Add untracked files into staging.
 // Automatically initialize if the repo is not initialized.
 func Add() {
+	if IsHelp() {
+		AddUsage()
+		return
+	}
+
 	args := os.Args[2:]
 
 	if NotGit() {
@@ -18,4 +26,17 @@ func Add() {
 	}
 
 	Git("status")
+}
+
+func AddUsage() {
+	fmt.Printf(`
+Add file contents to the index
+
+usage: %[1]s + [<file>]
+
+examples:
+    %[1]s + file.txt
+    %[1]s + file1.txt file2.txt
+    %[1]s + file1.txt dir2/file2.txt
+	`, Command)
 }
