439aa4f0c3ecf14330947eedec889a7cea7445d3
Salman S <salkuadrat@gmail.com>
2022-09-29 10:55:54 +0700

    edit functionality of command new

--- new.go
+++ new.go
@@ -1,5 +1,48 @@
 package git4humans
 
-func New() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func New() {
+	args := os.Args[2:]
+	dir := ""
+
+	if len(args) > 0 {
+		dir = args[0]
+	}
+
+	if len(dir) <= 0 {
+		fmt.Print("Working directory: ")
+
+		reader := bufio.NewReader(os.Stdin)
+		input, _, _ := reader.ReadLine()
+
+		if len(input) > 0 {
+			dir = strings.Replace(string(input), "\n", "", -1)
+		}
+	}
+
+	if len(dir) <= 0 {
+		fmt.Println("Error: you should specify a working directory for the new Git repository.")
+		return
+	}
+
+	err := os.Mkdir(dir, os.ModePerm)
+
+	if err != nil {
+		fmt.Println("Error: failed creating directory for the new Git repository.")
+		return
+	}
+
+	err = os.Chdir(dir)
+
+	if err != nil {
+		panic(err)
+	} else {
+		Git("init")
+	}
 }
