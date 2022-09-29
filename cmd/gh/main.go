64a5b935c60b1985d411b8ef669229c109d92993
Salman S <salkuadrat@gmail.com>
2022-09-29 18:09:33 +0700

    define each root command in main

--- cmd/gh/main.go
+++ cmd/gh/main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/git4humans/git4humans"
 
 func main() {
+	git4humans.Command = "gh"
 	git4humans.Handle()
 }
--- cmd/gt/main.go
+++ cmd/gt/main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/git4humans/git4humans"
 
 func main() {
+	git4humans.Command = "gt"
 	git4humans.Handle()
 }
