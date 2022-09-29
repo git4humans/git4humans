bff686311d16f285efc403f228388ffd9507be5a
Salman S <salkuadrat@gmail.com>
2022-09-29 18:29:26 +0700

    edit publish message

--- publish.go
+++ publish.go
@@ -80,7 +80,7 @@ func Publish() {
 		Git("status")
 		fmt.Println()
 
-		fmt.Println("You have unstaged changes in this project...")
+		fmt.Println("You have several changes in this project...")
 		fmt.Println()
 		fmt.Print("Save the changes? (y/n) ")
 
