621b0e78c2b8e5c39f545b54ede5fa0881005d0b
Salman S <salkuadrat@gmail.com>
2022-10-05 06:58:13 +0700

    update up command at RefineStatus

--- git.go
+++ git.go
@@ -76,7 +76,7 @@ func NoCommit() bool {
 }
 
 func RefineStatus(status string) string {
-	status = strings.ReplaceAll(status, `use "git push" to publish`, fmt.Sprintf(`use "%[1]s publish" or "%[1]s pub" to publish`, Command))
+	status = strings.ReplaceAll(status, `use "git push" to publish`, fmt.Sprintf(`use "%[1]s up" to upload`, Command))
 	status = strings.ReplaceAll(status, "git push", Command+" publish")
 	status = strings.ReplaceAll(status, "git add", Command+" +")
 	status = strings.ReplaceAll(status, "git rm", Command+" -")
