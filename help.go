14f2183f9b72db98c49075757e8e928565c2a68a
Salman S <salkuadrat@gmail.com>
2022-10-05 06:03:59 +0700

    update help text

--- help.go
+++ help.go
@@ -10,27 +10,26 @@ usage: %[2]s [<options>] <command> [<args]
 
 These are common commands you will most likely use in everyday situation.
 
-Start a working area
+Starting point
     copy  	Copy an existing Git repository to a new directory
     new		Create a new Git repository in a new directory 
     start	Start a new Git repository in the current working directory
     refresh     Refresh the existing Git repository in the current working directory
 
-Work on the current change 
+Managing changes 
     + 		Add file(s) to the staging area for inclusion in the next commit
     - 		Delete file(s), then stages the deletion for the next commit
-    move 	Move a file, a directory, or a symlink 
-    rename 	Rename a file, a directory, or a symlink 
-    restore	Restore deleted file(s)
-    save 	Record changes in your local repository
+    move 	Move a file, a directory, or a symlink (short: '%[2]s mv')
+    rename 	Rename a file, a directory, or a symlink (short: '%[2]s ren')
+    restore	Restore deleted file(s) (short: '%[2]s rs')
+    save 	Record changes in your local repository (short: '%[2]s sv')
 
-Collaborate with others
+Collaborations
     repo 	Manage remote repositories (list, add, delete, etc.)
-    fetch 	Fetch everything from a remote repository
-    sync 	Fetch from and integrate with a repository or a local branch 
-    publish     Publish changes to a remote repository (shortcut: '%[2]s pub')
+    sync 	Download from and integrate with a repository or a local branch 
+    up          Upload your local changes to a remote repository
 
-Grow, mark, and tweak your common history 
+Branches and history 
     branch 	Manage branches (list, create, delete, etc.) 
     switch 	Switch from your current branch to another branch 
     merge 	Join two or more branches
@@ -38,14 +37,16 @@ Grow, mark, and tweak your common history
     tag 	Create, list, delete, or verify a tag object
 
 Examine history and state 
-    status 	Show the status of the working copy (shortcut: '%[2]s s') 
-    log 	Show a reversed-ordered list of commits (shortcut: '%[2]s l')
-    diff 	Show changes between commits, commit and working tree, etc.
+    st 	    Show the status of the current working project
+    sst     Show the short status of the current working project 
+    lg 	    Show a reversed-ordered list of commits (logs)
+    slg     Show a list of commits in a short oneline format (short logs)
+    last    Show the last commit in the log history
 
 Configurations 
     config	Show or update configuration
     user	Show or update user configuration (local or global)
 
-You also can use the original commands from Git, e.g. '%[2]s push origin main' and 'git push origin main' can be used interchangeably in your project.
+You may also run all the original commands from Git. For example, '%[2]s push origin main' and 'git push origin main' can be used interchangeably in your project.
 	`, Version, Command)
 }
